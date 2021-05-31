8682a1d5fca175009e2cbf127f8f6eafcbe1895d
Minyi Chen <minyichen@MinyideMacBook-Pro.local>
2021-05-31 15:56:47 +0800

    update

--- Algorithm/ArrayAndLink/206_reverse-linked-list/247.java
+++ Algorithm/ArrayAndLink/206_reverse-linked-list/247.java
@@ -1,46 +1,26 @@
-import java.util.List;
-
-public class 247 {
-    public static class ListNode {
-        int val;
-        ListNode next;
-
-        ListNode() {
-        }
-
-        ListNode(int val) {
-            this.val = val;
-        }
-
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
-    }
-
-    public static void main(String[] args) {
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(5);
-
-        reverseList(head);
-    }
-
-    public static ListNode reverseList(ListNode head) {
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
         ListNode tempHead = null;
+        ListNode origin = head;
+        while (origin != null) {
+            ListNode temp = origin.next;
 
-        while (head != null) {
-            ListNode temp = head.next;
-            head.next = tempHead;
-            tempHead = head;
-            head = temp;
+            origin.next = tempHead;
+            tempHead = origin;
+            origin = temp;
         }
 
         return tempHead;
 
     }
-
 }
